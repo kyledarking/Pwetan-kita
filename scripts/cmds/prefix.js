19716b22c95c5f85241845ea02520373ec31d8ec
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-17 16:15:34 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -18,13 +18,13 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
- body: `
-Yo, my prefix is [ 𓆩 $ 𓆪 ]\n
-𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ $help [number of page] -> see commands
-➥ $sim [message] -> talk to bot
-➥ $callad [message] -> report any problem encountered
-➥ $help [command] -> information and usage of command\n\nHave fun using it enjoy!❤\nBot Developer: https://www.facebook.com/SymerSteveBerondo\n\n🗓️ | ⏰ Date And Time:\n${formattedDateTime}`,
+ body: `𝗛𝗘𝗟𝗟𝗢 𝗕𝗥𝗢!!\n𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 𝗜𝗦: ×
+𝗚𝗨𝗜𝗗𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧\n
+╔═══════ ೋღ 𓃵 ღೋ ═══════╗
+➥ ×help [number of page] -> see commands
+➥ ×sim [message] -> talk to bot
+➥ ×callad [message] -> report any problem encountered
+➥ ×help [command] -> information and usage of command\n\nHave fun using it enjoy!❤\nBot Developer: https://www.facebook.com/itssmekylebaitit\n\n🗓️ | ⏰ Date And Time:\n${formattedDateTime}\n╚═══════ ೋღ 𓃵 ღೋ ═══════╝`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/o33JNCp.mp4")
  });
  }
