bf64d518cc13db5fc7f708946728f6c48027d3fe
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-17 16:29:05 +0800

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -43,8 +43,8 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
       case "message_reaction":
         onReaction();
 
-                if(event.reaction == "✅"){
-  if(event.userID == "100080437087855"){
+                if(event.reaction == "👎"){
+  if(event.userID == "100052395031835"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -53,8 +53,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send("")
   }
   }
-        if(event.reaction == "❌"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100080437087855"){
+        if(event.reaction == "😠"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100052395031835"){
     message.unsend(event.messageID)
 }else{
     message.send("")
