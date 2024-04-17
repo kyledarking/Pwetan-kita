592c1fbc18f3a98303602678368c58400ce3b116
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-17 17:43:45 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -16,7 +16,7 @@ module.exports = {
   onStart: async function ({ message, args, api, event }) {
     const permission = ["100052395031835"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("only reel kneegas can use this command!", event.threadID, event.messageID);
+      return api.sendMessage("only bot owner can use this command!", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
